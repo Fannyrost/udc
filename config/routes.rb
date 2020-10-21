460fdd0e4a75c2eeef7704e40054a92aab804216
Fanny Rostaing <fanny@coffretis.com>
2020-10-21 16:27:33 +0200

    installation of tailwind, navbar + home banner ok

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
 
-  get 'productions/index'
+
   get '/presentation', to: 'pages#presentation', as: 'presentation'
   get '/gallery', to: 'pages#gallery', as: 'gallery'
   get '/saillie', to: 'pages#saillie', as: 'saillie'
   get '/contact', to: 'pages#contact', as: 'contact'
+  get '/production', to: 'productions#index', as: 'production'
 
-  resources :productions, only: [:index]
 
   root to: 'pages#home'
 end
