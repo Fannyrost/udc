9bb98f9eec90d831e1fdff78b0b450a87b254ca8
Fanny Rostaing <fanny@coffretis.com>
2020-10-21 13:13:13 +0200

    routes & model ok

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,16 @@
+class PagesController < ApplicationController
+  def home
+  end
+
+  def presentation
+  end
+
+  def gallery
+  end
+
+  def saillie
+  end
+
+  def contact
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  get 'productions/index'
+  get '/presentation', to: 'pages#presentation', as: 'presentation'
+  get '/gallery', to: 'pages#gallery', as: 'gallery'
+  get '/saillie', to: 'pages#saillie', as: 'saillie'
+  get '/contact', to: 'pages#contact', as: 'contact'
+
+  resources :productions, only: [:index]
+
+  root to: 'pages#home'
 end
